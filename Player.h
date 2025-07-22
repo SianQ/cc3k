a0bbbd5e2f97c786f6842ac5fa6fa250ec702355
Jingming Wu <j689wu@uwaterloo.ca>
2025-07-22 13:14:05 -0400

    Add RaceNames vector

--- Player.h
+++ Player.h
@@ -2,5 +2,15 @@
 
 // export
 
+#include <string>
+#include <vector>
+
 class Player {    
+    inline static const std::vector<std::string> RaceNames = {
+        "Human",
+        "Dwarf",
+        "Elf",
+        "Orc",
+        "Merchant"
+    };
 };
