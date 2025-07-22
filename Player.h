a6f91d2c2ef98e5a2b440ae3ce6d207c7cf5502c
Sian <153153657+SianQ@users.noreply.github.com>
2025-07-22 17:07:18 -0400

    Player Input specified

--- Player.h
+++ Player.h
@@ -26,10 +26,11 @@ public:
 
     static bool create(const std::string& race, std::unique_ptr<Player>) {
         std::string key = race;
-        if (key == "drow")    return std::make_unique<Drow>(new Drow());
-        else if (key == "vampire") return std::make_unique<Vampire>(new Vampire());
-        else if (key == "troll") return std::make_unique<Troll>(new Troll());
-        else if (key == "goblin") return std::make_unique<Goblin>(new Goblin());
-        else return std::make_unique<Shade>(new Shade());
+        if (key == "d" || key == "D")    return std::make_unique<Drow>(new Drow());
+        else if (key == "v" || key == "V") return std::make_unique<Vampire>(new Vampire());
+        else if (key == "t" || key == "T") return std::make_unique<Troll>(new Troll());
+        else if (key == "g" || key == "G") return std::make_unique<Goblin>(new Goblin());
+        else if (key == "s" || key == "S") return std::make_unique<Shade>(new Shade());
+        else return nullptr;
     }
 };
