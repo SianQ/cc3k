3ba154219ea6c618f36280db9a6ab78119737465
Sian <153153657+SianQ@users.noreply.github.com>
2025-07-22 14:05:57 -0400

    New PlayerFactory and basic Character
    Added empty PlayerFactory and basic fields and methods for Character.h

--- Character.h
+++ Character.h
@@ -11,4 +11,23 @@
 #include "Item.h"
 
 class Character {    
+protected:
+    int hp, atk, def;
+    int row, col;
+
+public:
+    Character(int hp, int atk, int def, int row, int col);
+    virtual ~Character();
+    
+    virtual int getHp() const;
+    virtual int getAtk() const;
+    virtual int getDef() const;
+    virtual void setPosition(int r, int c);
+    virtual std::pair<int, int> getPosition() const;
+
+    virtual void move(int row, int col);
+    virtual void takeDamage(Character* enemy);
+    virtual bool isDead();
+    virtual void pickUp(Item* item);
+    virtual void useItem(Item* item);
 };
