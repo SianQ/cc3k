8ff5cf882f6d70fab46aec2384662ec17a35e26b
Lydia Gao <l75gao@uwaterloo.ca>
2025-07-25 16:56:56 -0400

    Comment out legacy includes and update to module imports in multiple files

--- Item.h
+++ Item.h
@@ -4,9 +4,6 @@
 #include <string>
 #include "PotionType.h"
 
-// Forward declaration
-class Character;
-
 /**
  * Abstract base class: common interface for all items (gold, potions, etc.)
  */
---
+++ Stair.cc
@@ -0,0 +1,17 @@
+#include "Stair.h"
+
+bool isGold() {
+    return false;
+}
+
+bool isPotion() {
+    return false;
+}
+
+bool isStair() {
+    return true;
+}
+
+char getSymbol() {
+    return '\\';  // Display character for stairs
+}
