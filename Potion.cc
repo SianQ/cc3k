5c7e543776a20e382541bf1db6b6fa4d6e5223e5
Lydia Gao <l75gao@uwaterloo.ca>
2025-07-24 21:17:58 -0400

    refactor: remove unused use() method from Potion class, implement potion.cc

--- Potion.cc
+++ Potion.cc
@@ -1,21 +1,13 @@
+// Potion.cc
 #include "Potion.h"
+#include "Character.h"
 
-void Potion::use(Character& consumer) {
-    std::string type = this->getType();
+Potion::Potion(PotionType t) : type(t) {}
 
-    // Apply permanent HP effect directly
-    if (type == "RH" || type == "PH") {
-        this->use(consumer);
-    } else {
-        // Apply decorators for temporary effects
-        if (type == "BA") {
-            consumer = std::make_shared<BoostAtkDecorator>(consumer, 5);
-        } else if (type == "BD") {
-            consumer = std::make_shared<BoostDefDecorator>(consumer, 5);
-        } else if (type == "WA") {
-            consumer = std::make_shared<WoundAtkDecorator>(consumer, 5);
-        } else if (type == "WD") {
-            consumer = std::make_shared<WoundDefDecorator>(consumer, 5);
-        }
-    }
+char Potion::getSymbol() const {
+    return 'P'; // always displayed as 'P' on the map
+}
+
+PotionType Potion::getType() const {
+    return type;
 }
--- Potion.h
+++ Potion.h
@@ -17,9 +17,6 @@ class Potion : public Item
 public:
     Potion(PotionType t);
 
-    // Apply the potion effect to the consumer and mark discovered = true
-    void use(Character &consumer) override;
-
     // If discovered, returns the specific name (e.g. "Restore Health Potion"),
     // otherwise returns "Unknown Potion"
     char getSymbol() const override;
